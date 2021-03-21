59a1567d95f491897bf168ef0c92f9a9566005bc
Hirusha-H <77770753+Hirusha-H@users.noreply.github.com>
2021-03-21 18:50:53 +0530

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -16,7 +16,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="Type a YouTube video name... by Nexa Bots",
             switch_pm_parameter="help",
             cache_time=0
         )
