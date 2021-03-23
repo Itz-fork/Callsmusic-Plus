96d3c83f27e2e6cca33a421234f982ad52de216e
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-03-23 19:45:21 +0530

    itz beta

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,15 @@
+import queue
+
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
+import callsmusic
+
+import queues
+import cache.admins
+
 from helpers.filters import command, other_filters, other_filters2
+from helpers.wrappers import errors, admins_only
 
 
 @Client.on_message(command("start") & other_filters2)
@@ -251,3 +259,67 @@ async def promode(_, message: Message):
             ]
         )
     )
+    
+    
+    
+@Client.on_message(command(["pause", "p"]))
+@errors
+@admins_only
+async def pause(_, message: Message):
+    if (
+            message.chat.id not in callsmusic.pytgcalls.active_calls
+    ) or (
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
+    ):
+        await message.reply_text("❕ Nothing is playing.")
+    else:
+        callsmusic.pytgcalls.pause_stream(message.chat.id)
+        await message.reply_text("⏸ Paused.")
+
+
+@Client.on_message(command(["resume", "r"]))
+@errors
+@admins_only
+async def resume(_, message: Message):
+    if (
+            message.chat.id not in callsmusic.pytgcalls.active_calls
+    ) or (
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
+    ):
+        await message.reply_text("❕ Nothing is paused.")
+    else:
+        callsmusic.pytgcalls.resume_stream(message.chat.id)
+        await message.reply_text("▶️ Resumed.")
+
+
+@Client.on_message(command(["stop", "s"]))
+@errors
+@admins_only
+async def stop(_, message: Message):
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❕ Nothing is streaming.")
+    else:
+        try:
+            queues.clear(message.chat.id)
+        except queue.Empty:
+            pass
+
+        callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        await message.reply_text("⏹ Stopped streaming.")
+
+
+@Client.on_message(command(["skip", "f"]))
+@errors
+@admins_only
+async def skip(_, message: Message):
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❕ Nothing is playing to skip.")
+    else:
+        queues.task_done(message.chat.id)
+
+        if queues.is_empty(message.chat.id):
+            callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        else:
+            callsmusic.pytgcalls.change_stream(message.chat.id, queues.get(message.chat.id)["file_path"])
+
+        await message.reply_text("⏩ Skipped the current song.")
