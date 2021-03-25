d81ff31475e7cbb74bdca4fa3b85933b3d9f8b3b
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-03-25 15:30:21 +0530

    it's su projects!

--- helpers/filters.py
+++ helpers/filters.py
@@ -10,4 +10,3 @@ other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filt
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
-
