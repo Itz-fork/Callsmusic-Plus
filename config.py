bcacaea52c205733e4e0dc47699c4cdb392e7e00
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-03-27 16:04:51 +0530

    why this tho

--- config.py
+++ config.py
@@ -15,3 +15,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
