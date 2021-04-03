06d28110786ba9aeb3f5563869199fb4d1f491b4
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-03 18:37:09 +0530

    why

--- handlers/chat_member_updated.py
+++ handlers/chat_member_updated.py
@@ -6,12 +6,24 @@ from cache import admins as cache
 
 @Client.on_chat_member_updated()
 async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
-    chat = chat_member_updated.chat.id
-    new = chat_member_updated.new_chat_member
-
-    if new.can_manage_voice_chats:
-        if new.user.id not in cache.admins[chat]:
-            cache.admins[chat].append(new.user.id)
-    else:
-        if new.user.id in cache.admins[chat]:
-            cache.admins[chat].remove(new.user.id)
+    (
+        admins.admins[chat_member_updated.chat.id].append(
+            chat_member_updated.new_chat_member.user.id
+        )
+    ) if (
+        (
+            chat_member_updated.new_chat_member.can_manage_voice_chats
+        ) and (
+            (
+                chat_member_updated.new_chat_member.user.id
+            ) not in admins.admins[chat_member_updated.chat.id]
+        )
+    ) else (
+        admins.admins[chat_member_updated.chat.id].remove(
+            chat_member_updated.new_chat_member.user.id
+        )
+    ) if (
+        (
+            chat_member_updated.new_chat_member.user.id
+        ) in admins.admins[chat_member_updated.chat.id]
+    ) else None
