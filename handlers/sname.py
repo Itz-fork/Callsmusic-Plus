ff7fc2a3bf2be270a5de37355f26d9c805218137
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-05 15:49:07 +0530

    username not found

--- handlers/sname.py
+++ handlers/sname.py
@@ -53,7 +53,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Your Song Is Successfully Uploaded 😌️! Join @NexaBotsUpdates ❤️** . Song Requested By {message.from_user.first_name} 🙂️'
+        rep = '**Your Song Is Successfully Uploaded 😌️! Join @NexaBotsUpdates ❤️** 🙂️'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
