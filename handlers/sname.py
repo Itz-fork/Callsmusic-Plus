316b06d2e9a9d613cddb4f9f6da5343126af8ada
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-05 15:24:48 +0530

    thanks to ImJanindu

---
+++ handlers/sname.py
@@ -0,0 +1,72 @@
+# A Powerful Plugin by @Mr_Dark_Prince
+# Copied From A Sexy Project From Infinity BOTs <https://t.me/Infinity_BOTs>
+
+import os
+import requests
+import aiohttp
+import youtube_dl
+
+from JESongBot import Jebot as app
+from pyrogram import filters, Client
+from youtube_search import YoutubeSearch
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+@app.on_message(filters.command('yts'))
+def song(client, message):
+
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
+
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('Please wait... I am searching for your song! 😊️. **Join @NexaBotsUpdates**')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+
+    except Exception as e:
+        m.edit(
+            "Hmmm... I can't find anything! 😊️.\n\nMaybe Your Spellings Wrong? 😐️"
+        )
+        print(str(e))
+        return
+    m.edit("__Your Song is Downloading...__  😉️ Do join **@NexaBotsUpdates**")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = '**Your Song Is Successfully Uploaded 😌️! Join @NexaBotsUpdates ❤️** . Song Requested By {message.from_user.first_name} 🙂️'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ Error')
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
