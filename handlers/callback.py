95ed6ac11f51057a7a57696ec9c0648ebda500dd
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-09 14:08:51 +0530

    re

--- handlers/callback.py
+++ handlers/callback.py
@@ -19,9 +19,9 @@ async def close(_, query: CallbackQuery):
 @authorized_users_only
 async def pause(_, query: CallbackQuery):
     if callsmusic.pause(message.chat.id):
-        await query.message.edit_text("⏸ Paused")
+        await query.message.reply_text("⏸ Paused")
     else:
-        await query.edit_text("❗️ Nothing is playing")
+        await query.reply_text("❗️ Nothing is playing")
 
 
 @Client.on_callback_query(filters.regex("resume"))
@@ -29,9 +29,9 @@ async def pause(_, query: CallbackQuery):
 @authorized_users_only
 async def resume(_, query: CallbackQuery):
     if callsmusic.resume(message.chat.id):
-        await query.message.edit_text("🎧 Resumed")
+        await query.message.reply_text("🎧 Resumed")
     else:
-        await query.message.edit_text("❗️ Nothing is paused")
+        await query.message.reply_text("❗️ Nothing is paused")
 
 
 @Client.on_callback_query(filters.regex("stop"))
@@ -39,7 +39,7 @@ async def resume(_, query: CallbackQuery):
 @authorized_users_only
 async def stop(_, query: CallbackQuery):
     if message.chat.id not in callsmusic.active_chats:
-        await query.message.edit_text("❗️ Nothing is playing")
+        await query.message.reply_text("❗️ Nothing is playing")
     else:
         try:
             queues.clear(message.chat.id)
@@ -47,7 +47,7 @@ async def stop(_, query: CallbackQuery):
             pass
 
         await callsmusic.stop(message.chat.id)
-        await query.message.edit_text("✅ Cleared the queue and left the Voice Chat!")
+        await query.message.reply_text("✅ Cleared the queue and left the Voice Chat!")
 
 
 @Client.on_callback_query(filters.regex("skip"))
@@ -55,7 +55,7 @@ async def stop(_, query: CallbackQuery):
 @authorized_users_only
 async def skip(_, query: CallbackQuery):
     if message.chat.id not in callsmusic.active_chats:
-        await query.message.edit_text("❗️ Nothing is playing")
+        await query.message.reply_text("❗️ Nothing is playing")
     else:
         queues.task_done(message.chat.id)
 
@@ -66,7 +66,7 @@ async def skip(_, query: CallbackQuery):
                 message.chat.id, queues.get(message.chat.id)["file"]
             )
 
-        await query.message.edit_text("Skipped.")
+        await query.message.reply_text("Skipped.")
 
 
 @Client.on_callback_query(filters.regex("mute"))
@@ -76,11 +76,11 @@ async def mute(_, query: CallbackQuery):
     result = callsmusic.mute(message.chat.id)
 
     if result == 0:
-        await query.message.edit_text("🔇 Muted")
+        await query.message.reply_text("🔇 Muted")
     elif result == 1:
-        await query.message.edit_text("🔇 Already muted")
+        await query.message.reply_text("🔇 Already muted")
     elif result == 2:
-        await query.message.edit_text("❗️ Not in voice chat")
+        await query.message.reply_text("❗️ Not in voice chat")
 
 
 @Client.on_callback_query(filters.regex("unmute"))
@@ -90,8 +90,8 @@ async def unmute(_, query: CallbackQuery):
     result = callsmusic.unmute(message.chat.id)
 
     if result == 0:
-        await query.message.edit_text("🔈 Unmuted")
+        await query.message.reply_text("🔈 Unmuted")
     elif result == 1:
-        await query.message.edit_text("🔈 Already unmuted")
+        await query.message.reply_text("🔈 Already unmuted")
     elif result == 2:
-        await query.message.edit_text("❗️ Not in voice chat")
+        await query.message.reply_text("❗️ Not in voice chat")
