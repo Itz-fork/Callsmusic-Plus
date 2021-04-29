ffdee0b0e6dabe2674ff2cdece350c7731255d4d
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-29 17:52:15 +0530

    some emojies !

--- handlers/play.py
+++ handlers/play.py
@@ -13,27 +13,34 @@ from helpers.errors import DurationLimitError
 from helpers.filters import command, other_filters
 from helpers.decorators import errors
 
-
 @Client.on_message(command("play") & other_filters)
 @errors
 async def play(_, message: Message):
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
 
-    res = await message.reply_text("🔄 Processing...")
+    response = await message.reply_text("**Processing Your Song...** 😇")
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"Videos longer than {DURATION_LIMIT} minutes aren't allowed, the provided video is {audio.duration / 60} minutes."
+                f"Bruh! Videos longer than {DURATION_LIMIT} minute(s) aren’t allowed, the provided audio is {round(audio.duration / 60)} minute(s) 😒"
             )
 
         file_name = audio.file_unique_id + "." + (
-            audio.file_name.split(
-                ".")[-1] if not isinstance(audio, Voice) else "ogg"
+            (
+                audio.file_name.split(".")[-1]
+            ) if (
+                not isinstance(audio, Voice)
+            ) else "ogg"
         )
+
         file = await converter.convert(
-            (await message.reply_to_message.download(file_name))
-            if not path.isfile(path.join("downloads", file_name)) else file_name
+            (
+                await message.reply_to_message.download(file_name)
+            )
+            if (
+                not path.isfile(path.join("downloads", file_name))
+            ) else file_name
         )
     else:
         messages = [message]
@@ -56,7 +63,7 @@ async def play(_, message: Message):
                         break
 
         if offset in (None,):
-            await res.edit_text("❗️ You did not give me anything to play.")
+            await response.edit_text("Lol! `You did not give me anything to play!`")
             return
 
         url = text[offset:offset + length]
@@ -64,7 +71,7 @@ async def play(_, message: Message):
 
     if message.chat.id in callsmusic.active_chats:
         position = await queues.put(message.chat.id, file=file)
-        await res.edit_text(f"*️⃣ Queued at position {position}")
+        await response.edit_text(f"**Your Song Queued at position {position}!**")
     else:
-        await res.edit_text("🎧 Playing...")
         await callsmusic.set_stream(message.chat.id, file)
+        await response.edit_text("**Playing Your Song...** 🎧")
