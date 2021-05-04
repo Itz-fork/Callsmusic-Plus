cf576af90fb4ea2fce8e331005c67ba5ef25dd05
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-05-04 16:51:56 +0530

    eh sed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client as Bot, filters
 
 from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, SUDO_USERS
 
 
 bot = Bot(
