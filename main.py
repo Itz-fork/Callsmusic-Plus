af768bfd4fa346e8e9bad5eaebd1f3c0e66d5c94
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-05-04 16:33:36 +0530

    lel

--- main.py
+++ main.py
@@ -12,5 +12,17 @@ bot = Bot(
     plugins=dict(root="handlers")
 )
 
+## Fucked Command ##
+
+@Bot.on_message(filters.command("update") & filters.user(SUDOERS))
+async def update_restart(_, message):
+    await send(
+        f'```{subprocess.check_output(["git", "pull"]).decode("UTF-8")}```'
+    )
+    os.execvp(
+        f"python{str(pyver.split(' ')[0])[:3]}",
+        [f"python{str(pyver.split(' ')[0])[:3]}", "main.py"],
+    )
+
 bot.start()
 run()
