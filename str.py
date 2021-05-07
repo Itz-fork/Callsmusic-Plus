f8f80a10efd59d61f9c8a883a30a96e4eee17726
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-05-07 13:22:23 +0530

    nice

--- str.py
+++ str.py
@@ -1,9 +1,26 @@
+# Created By CallsMusic
+
 import asyncio
+import tgcrypto
 
 from pyrogram import Client
 
-
-print("Enter your app information from my.telegram.org/apps below.")
+print("Made For")
+print("")
+print('''
+ / ___|__ _| | |___|  \/  |_   _ ___(_) ___     |  _ \| |_   _ ___ 
+| |   / _` | | / __| |\/| | | | / __| |/ __|____| |_) | | | | / __|
+| |__| (_| | | \__ \ |  | | |_| \__ \ | (_|_____|  __/| | |_| \__ \.
+ \____\__,_|_|_|___/_|  |_|\__,_|___/_|\___|    |_|   |_|\__,_|___/
+''')
+print("")
+print('''
+ /\  /  _  _ |  | _ | _  _ .__.._ _  \  //  |\/|    _o _ |_)| _.   _ ._| 
+/--\ \_(_)(_)|  |(/_|(/_(_||(_|| | |  \/ \_ |  ||_|_>|(_ |  |(_|\/(/_| o 
+                         _|                                     /        
+''')
+print("")
+print("Enter All Required Things To Generate String Session.")
 
 
 async def main():
