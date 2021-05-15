2eddf966154402544046a61d0b5f25eb45f5a29d
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-05-15 10:43:22 +0530

    removed shit

--- helpers/channelmusic.py
+++
@@ -1,9 +0,0 @@
-# (c) TeamDaisyX
-
-from pyrogram.types import Chat
-
-
-def get_chat_id(chat: Chat):
-    if chat.title.startswith("Channel Music: ") and chat.title[16:].isnumeric():
-        return int(chat.title[15:])
-    return chat.id
