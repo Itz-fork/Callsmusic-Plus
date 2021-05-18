d69a7a9a28bf3a6184b54cfc6bc2f3bfc21d0ede
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-05-18 10:37:24 +0530

    enter in a nutshell

--- handlers/play.py
+++ handlers/play.py
@@ -75,9 +75,9 @@ async def play(_, message: Message):
         position = await queues.put(message.chat.id, file=file)
         MENTMEH = message.from_user.mention()
         await response.delete()
-        await message.reply_photo(thumb, caption=f"**Your Song Queued at position** {position}! **Requested by: {MENTMEH}**")
+        await message.reply_photo(thumb, caption=f"**Your Song Queued at position** {position}! \n **Requested by: {MENTMEH}**")
     else:
         thumb = "https://telegra.ph/file/a4b7d13da17c3cc828ab9.jpg"
         await callsmusic.set_stream(message.chat.id, file)
         await response.delete()
-        await message.reply_photo(thumb, caption="**Playing Your Song 🎧...** **Requested by: {}**".format(message.from_user.mention()))
+        await message.reply_photo(thumb, caption="**Playing Your Song 🎧...** \n **Requested by: {}**".format(message.from_user.mention()))
