1c5599644f8f90506431770e768d5bb9cccdd424
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-18 18:19:57 +0530

    bye bye shit

--- handlers/play.py
+++ handlers/play.py
@@ -24,16 +24,6 @@ async def _(bot: Client, cmd: command):
 @Client.on_message(command(["play", "play@MusicsNexa_bot"]) & other_filters)
 @errors
 async def play(_, message: Message):
-    usr_cmd = message.text.split("_")[-1]
-    if usr_cmd == "/play":
-        chat_id = message.from_user.id
-        if not await db.is_user_exist(chat_id):
-            await db.add_user(chat_id)
-            await Client.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"**📢 News ** \n#New_Music_Lover **Started To Using Meh!** \n\nFirst Name: `{message.from_user.first_name}` \nUser ID: `{message.from_user.id}` \nProfile Link: [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
-        parse_mode="markdown"
-    )
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
 
     response = await message.reply_text("**Processing Your Song 😇...**")
