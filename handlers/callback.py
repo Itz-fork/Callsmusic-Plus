22e9972dee9b08edfe31e1ea792a7902db22bfeb
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-19 20:45:40 +0530

    Added Callbacks For New Help Menu

--- handlers/callback.py
+++ handlers/callback.py
@@ -12,12 +12,15 @@ async def close(_, query: CallbackQuery):
 
 # Start callback 
 
-@Client.on_callback_query(filters.regex("startcb"))
+@Client.on_callback_query(filters.regex("cbstart"))
 async def startcb(_, query: CallbackQuery):
     await query.edit_message_text(
-        f"""<b>Hey 😉️!</b>
-I'm The Nexa Music Bot. Friend of **@{FRIEND_BOT}** 😏️.
-I can play Music In Telegram Groups Via Voice Chat! 😌️.
+        f"""<b>Hi {query.message.from_user.mention} 😉️!</b>
+
+I'm The Nexa Music Bot! A Powerful Bot to Play Music in Your Group Voice Chat 😇!
+
+Also I have more features! Please hit on **/help** to see them 😘!
+
 Made with ❤️ <b>@NexaBotsUpdates</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -28,7 +31,7 @@ Made with ❤️ <b>@NexaBotsUpdates</b>""",
                 ],
                 [
                     InlineKeyboardButton(
-                        "🤨️ How To Use Me 🤨️", url="https://telegra.ph/How-To-Use-Music-Nexa-Bot-03-16"
+                        "👮‍♂️ Help Menu 👮‍♂️", callback_data="cbhelpmenu"
                     )
                 ],
                 [
@@ -44,48 +47,246 @@ Made with ❤️ <b>@NexaBotsUpdates</b>""",
     )
     
 
-# Command list callback
+# Help Callback Menu
 
-@Client.on_callback_query(filters.regex("cmdlistcb"))
-async def cmdlistcb(_, query: CallbackQuery):
+@Client.on_callback_query(filters.regex("cbhelpmenu"))
+async def cbhelpmenu(_, query: CallbackQuery):
     await query.edit_message_text(
-        f"""<b>Hey 😉️!</b>
+        f"""<b>Hi {query.message.from_user.mention} 😉️!</b>
 
-Here is the list of available commands! 😃️
+**Here is the Help Menu For This Bot 😊!**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🤔 How To Use Me 🤔", callback_data="cbhowtouse"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Get Lyrics", callback_data="cbgetlyrics"
+                    ),
+                    InlineKeyboardButton(
+                        "YT Search", callback_data="cbytsearch"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Music Downloader", callback_data="cbmusicdown"
+                    ),
+                    InlineKeyboardButton(
+                        "YT Video Downloader", callback_data="cbytviddown"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Delete Commands", callback_data="cbdelcmds"
+                    ),
+                    InlineKeyboardButton(
+                        "Quotely", callback_data="cbquotely"
+                    )
+                ]
+            ]
+        )
+    )
 
-• **Group Admin Only Commands 👮 ✓,**
 
- ➲ <code>/play</code> - Reply to supported url or "/play supported url"
- ➲ <code>/skip</code> - Skip currenly playing song!
- ➲ <code>/pause</code> - Pause currently playing song!
- ➲ <code>/resume</code> - Resume currently pushed song!
- ➲ <code>/mute</code> - Mutes Streamer!
- ➲ <code>/unmute</code> - Unmutes streamer!
- ➲ <code>/joingrp</code> - To Add Streamer Account To Your Group!
- ➲ <code>/leavegrp</code> - To Remove Streamer Account From Your Group!
+# Lyrics Module Help
 
+@Client.on_callback_query(filters.regex("cbgetlyrics"))
+async def cbgetlyrics(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""<b>Help For Lyrics Plugin</b>
 
-• **Group Members Commands 👮 ✓,**
+**Feature:** Get Lyrics For Provided Song Name!
 
- ➲ <code>/vc</code> - Give voice chat link of your group! (Only For Public Groups)
- ➲ <code>/yts (song name)</code> - Download song by it's name!
- ➲ <code>/ytvid (song name)</code> - Download Videos From YouTube!
- ➲ <code>/saavn (song name)</code> - Download Songs From Saavn!
- ➲ <code>/deezer (song namme)</code> - Download Songs From Deezer!
+**Usage:**
+    - Send Your Song Name with `/lyrics` command.
 
-**❌ Don't End Voice Chat While Bot Playing A Song ❌**
+**Example:** `/lyrics faded`
 
 Made with ❤️ by **@NexaBotsUpdates**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "👀️ Supported Sites 👀️", url="https://ytdl-org.github.io/youtube-dl/supportedsites.html"
+                        "◀️ Back ◀️", callback_data="cbhelpmenu"
                     )
-                ],
+                ]
+            ]
+        )
+    )
+
+
+# Yt Search Module Help
+
+@Client.on_callback_query(filters.regex("cbytsearch"))
+async def cbytsearch(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""<b>Help For YT Search Plugin</b>
+
+**Feature:** Search Youtube Videos Inline or Using a Command!
+
+**Usage:**
+    1. For Inline Search Feature,
+     - Type `@MusicsNexa_Bot` in any chat then type ` `(space) and search.
+    
+    2. For Search Via Command,
+     - Send `/ytsearch` command with your keyword.
+
+**Example:**
+    1. Example For Inline Search
+     - `@MusicsNexa_Bot faded`
+    
+    2. Example For Search via Command
+     - `/ytsearch faded`
+
+Made with ❤️ by **@NexaBotsUpdates**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
                 [
                     InlineKeyboardButton(
-                        "⟲ Go Back ⟲", callback_data="startcb"
+                        "◀️ Back ◀️", callback_data="cbhelpmenu"
+                    )
+                ]
+            ]
+        )
+    )
+    
+    
+# Music Downloader Help
+
+@Client.on_callback_query(filters.regex("cbmusicdown"))
+async def cbmusicdown(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""<b>Help For Music Downloader Plugin</b>
+
+**Feature:** Download Music As Audio From YouTube, Saavn, Deezer
+
+**Usage:**
+    1. For Youtube Audio Download,
+      - Send Your Song Name with `/yts` command.
+    
+    2. For Saavn Audio Download,
+      - Send Your Song Name with `/saavn` command.
+    
+    3. For Deezer Audio Download,
+      - Send Your Song Name with `/deezer` command.
+
+**Example:**
+    1. Example For Youtube Audio Download,
+      - `/yts alone`
+    
+    2. Example For Saavn Audio Download,
+      - `/saavn faded`
+    
+    3. Example For Deezer Audio Download,
+      - `/deezer unity`
+
+Made with ❤️ by **@NexaBotsUpdates**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "◀️ Back ◀️", callback_data="cbhelpmenu"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+# YT Video Downloader Help
+
+@Client.on_callback_query(filters.regex("cbytviddown"))
+async def cbytviddown(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""<b>Help For YT Video Downloader Plugin</b>
+
+**Feature:** Download Youtube Videos For Provided Name!
+
+**Usage:**
+    - Send Your Youtube Video Name with `/ytvid` command.
+
+**Example:** `/ytvid faded`
+
+Made with ❤️ by **@NexaBotsUpdates**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "◀️ Back ◀️", callback_data="cbhelpmenu"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+# Delete Command Help
+
+@Client.on_callback_query(filters.regex("cbdelcmds"))
+async def cbdelcmds(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""<b>Help For Delete Command Plugin</b>
+
+**Feature:** Delete Commands Sent By Users Releated To This Bot!
+
+**Usage:**
+    1. To Turn On This,
+      - Send `/delcmd on` command.
+    
+    2. To Turn Off This,
+      - Send `/delcmd off` command.
+
+Made with ❤️ by **@NexaBotsUpdates**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "◀️ Back ◀️", callback_data="cbhelpmenu"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+# Quotely Help
+
+@Client.on_callback_query(filters.regex("cbquotely"))
+async def cbquotely(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""<b>Help For Quotely Plugin</b>
+
+**Feature:** Quote Messages Like Quotely Bot!
+
+**Usage:**
+    1. To Quote One Message,
+      - `/q` reply to a text message
+      
+    2. To Quote More Than One Message,
+      - `/q` [Integer] reply to a text message
+     
+    3. To Quote Message with Reply
+      - `/q r` reply to a text message
+
+**Example:**
+    1. Example Quote One Message,
+      - `/q` reply to a text message
+      
+    2. Example Quote More Than One Message,
+      - `/q 2` reply to a text message
+     
+    3. Example Quote Message with Reply,
+      - `/q r` reply to a text message
+
+Made with ❤️ by **@NexaBotsUpdates**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "◀️ Back ◀️", callback_data="cbhelpmenu"
                     )
                 ]
             ]
