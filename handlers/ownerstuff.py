97c279417c7339581f6cf4003b4beba62fcfc235
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-21 14:05:23 +0530

    added some bugs

--- handlers/ownerstuff.py
+++ handlers/ownerstuff.py
@@ -1,3 +1,5 @@
+# Copyright (c) DevsExspo (Friday UserBot)
+
 import os
 import shlex
 import asyncio
@@ -8,7 +10,6 @@ from typing import Tuple
 
 
 async def execute(cmd: str) -> Tuple[str, str, int, int]:
-    """Run Commands"""
     args = shlex.split(cmd)
     process = await asyncio.create_subprocess_exec(
         *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -24,4 +25,4 @@ async def execute(cmd: str) -> Tuple[str, str, int, int]:
 # Command to update yt-dl
 @Client.on_message(filters.command(["uytdl", "uytdl@MusicsNexa_bot"]))
 async def uytdl(_, message: Message):
-  await execute("sudo youtube-dl -U")
+  await execute("youtube-dl -U")
