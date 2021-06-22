4ee8bd193547e09c1de13f0c48da593f107efe35
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-22 08:45:04 +0530

    Bugs

--- handlers/musicdwn.py
+++ handlers/musicdwn.py
@@ -26,7 +26,7 @@ from Python_ARQ import ARQ
 
 from config import ARQ_API_URL, ARQ_API_KEY
 from helpers.merrors import capture_err
-from helpers.modhelps import paste
+from helpers.modhelps import paste, downloader
 
 is_downloading = False
 
