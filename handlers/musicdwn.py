ec42b60e123a62024a4ea44267ad45f19672eab5
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-22 11:29:42 +0530

    Removed yt download for temp fix

--- handlers/musicdwn.py
+++ handlers/musicdwn.py
@@ -13,7 +13,6 @@ import requests
 import wget
 import youtube_dl
 
-from pykeyboard import InlineKeyboard
 from asyncio import gather
 from os import remove
 from random import randint
@@ -32,17 +31,6 @@ is_downloading = False
 aiohttpsession = ClientSession()
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
-
-VIDEO_DATA = {}
-
-# Convert seconds to mm:ss
-async def timeFormat(seconds: int):
-    seconds = int(seconds)
-    seconds = seconds % (24 * 3600)
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60
-    return "%02d:%02d" % (minutes, seconds)
 
 
 @Client.on_message(filters.command(['yts', 'yts@MusicsNexa_Bot']))
@@ -219,102 +207,3 @@ async def lyrics_func(_, message):
         return
     lyrics = await paste(lyrics)
     await m.edit(f"**Oops! Lyrics Too Long To Send!** \n**Your Song Lyrics: [Click Here]({lyrics})**")
-    
-    
-    
-# Youtube Video Download
-
-@Client.on_message(filters.command("ytvid"))
-@capture_err
-async def ytvidz(_, message):
-    if len(message.command) != 2:
-        return await message.reply_text("Lol! Wrong Way bro! Read **/help** section!")
-    m = await message.reply_text("`Processing...`")
-    url = message.text.split(None, 1)[1]
-    results = await arq.ytdl(url)
-    if not results.ok:
-        return await m.edit(results.result)
-    result = results.result
-    title = result.title
-    thumbnail = result.thumbnail
-    duration = result.duration
-    video = result.video
-    buttons = InlineKeyboard(row_width=3)
-    keyboard = []
-    for media in video:
-        quality = media.quality
-        size = media.size
-        url = media.url
-        format = media.format
-        data = str(randint(999, 9999999))
-        VIDEO_DATA[data] = {
-            "url": url,
-            "title": title,
-            "quality": quality,
-            "duration": duration,
-            "format": format,
-            "thumbnail": thumbnail,
-            "cc": message.from_user.mention if message.from_user else "Anon",
-        }
-        keyboard.append(
-            InlineKeyboardButton(
-                text=f"{quality} | {size}", callback_data=f"ytdl {data}"
-            )
-        )
-    buttons.add(*keyboard)
-    caption = f"""
-**Title:** {title}
-**Duration:** {await timeFormat(duration)}
-"""
-    await message.reply_photo(thumbnail, caption=caption, reply_markup=buttons)
-    await m.delete()
-
-
-@Client.on_callback_query(filters.regex(r"^ytdl"))
-async def ytdlCallback(_, cq):
-    await cq.message.edit("`Downloading...`")
-    data_ = cq.data.split()[1]
-    try:
-        data = VIDEO_DATA[data_]
-        url = data["url"]
-        title = data["title"]
-        duration = data["duration"]
-        format = data["format"]
-        thumbnail = data["thumbnail"]
-        cc = data["cc"]
-        caption = f"""
-**Title:** `{title}`
-**File Format:** `{format}`
-**Duration:** `{await timeFormat(duration)}`
-**Requested by:** `{cc}`
-        """
-        media, thumb = await gather(
-            downloader.download(url), downloader.download(thumbnail)
-        )
-        await cq.message.edit("`Uploading...`")
-        if format == "mp3":
-            await cq.message.reply_audio(
-                media,
-                quote=False,
-                caption=caption,
-                duration=duration,
-                thumb=thumb,
-                title=title,
-            )
-        else:
-            await cq.message.reply_video(
-                media,
-                caption=caption,
-                quote=False,
-                duration=duration,
-                supports_streaming=True,
-            )
-        del VIDEO_DATA[data_]
-        remove(thumb)
-        remove(media)
-        await cq.message.delete()
-    except Exception as e:
-        e = traceback.format_exc()
-        print(e)
-        del VIDEO_DATA[data_]
-        await cq.message.delete()
