fb29ef1f6fbb6a3639adec77c77ee99ec05a7cf8
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-23 17:52:21 +0530

    sed

--- helpers/modhelps.py
+++ helpers/modhelps.py
@@ -1,6 +1,7 @@
 # This File Is Made and Owned By @TheHamkerCat and @TeamDaisyX
 
 import socket
+import heroku3
 
 
 async def _netcat(host, port, content):
@@ -19,3 +20,25 @@ async def _netcat(host, port, content):
 async def paste(content):
     link = await _netcat("ezup.dev", 9999, content)
     return link
+
+
+# Updator
+
+def fetch_heroku_git_url(api_key, app_name):
+    if not api_key:
+        return None
+    if not app_name:
+        return None
+    heroku = heroku3.from_key(api_key)
+    try:
+        heroku_applications = heroku.apps()
+    except:
+        return None
+    heroku_app = None
+    for app in heroku_applications:
+        if app.name == app_name:
+            heroku_app = app
+            break
+    if not heroku_app:
+        return None
+    return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
