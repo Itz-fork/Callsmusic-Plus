a9b51fc55a8bebcfcbeb05534845acfda0e9c5d1
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-06-24 12:19:31 +0530

    Test

--- handlers/musicdwn.py
+++ handlers/musicdwn.py
@@ -35,7 +35,7 @@ is_downloading = False
 
 aiohttpsession = ClientSession()
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
-
+BOT_USERNAME = (await Client.get_me()).username
 
 def get_file_extension_from_url(url):
     url_path = urlparse(url).path
@@ -160,7 +160,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: `YouTube`\n⏱️ **Song Duration**: `{duration}`\n👁‍🗨 **Song Views**: `{views}`\n**Uploaded By**: **@MusicsNexa_Bot** \n **Join @NexaBotsUpdates 😉** '
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: `YouTube`\n⏱️ **Song Duration**: `{duration}`\n👁‍🗨 **Song Views**: `{views}`\n**Uploaded By**: **@{BOT_USERNAME}** \n **Join @NexaBotsUpdates 😉** '
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
