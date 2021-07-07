7f89a1d4ad5c97703d97a5f5c6113c8208d0a337
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-07 20:54:03 +0530

    fix

--- str.py
+++ str.py
@@ -30,7 +30,7 @@ print("Enter Your APP ID and API HASH To Generate Pyrogram String Session.")
 async def main():
     async with Client(":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")) as app:
         PYRO_SESSION = await app.export_session_string()
-    await app.send_message("me", f"**Pyrogram String Session:** \n{PYRO_SESSION} \n\n**Powered by @NexaBotsUpdates 😇")
+        await app.send_message("me", f"**Pyrogram String Session:** \n{PYRO_SESSION} \n\n**Powered by @NexaBotsUpdates 😇")
         print(f"Here is your Pyrogram String Session: \n {PYRO_SESSION} \n\nPro tip: Check your saved message also, Backup of this session is also saved in there :)")
 
 
