d1317d230f051fa83c84549284b1fb3d24af5b22
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-07 21:07:50 +0530

    Some fixes and improvements :)

--- str.py
+++ str.py
@@ -1,6 +1,7 @@
 # Created By CallsMusic
 
 import asyncio
+import time
 import tgcrypto
 
 from pyrogram import Client
@@ -23,14 +24,14 @@ Github: https://github.com/Itz-fork/Callsmusic-Plus
 Docs: https://itz-fork.gitbook.io/callsmusic-plus
 Website: https://musicsnexabot.netlify.app
 """)
-asyncio.sleep(5) # Just for show off
+time.sleep(5) # Just for show off
 print("Enter Your APP ID and API HASH To Generate Pyrogram String Session.")
 
 
 async def main():
     async with Client(":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")) as app:
         PYRO_SESSION = await app.export_session_string()
-        await app.send_message("me", f"**Pyrogram String Session:** \n{PYRO_SESSION} \n\n**Powered by @NexaBotsUpdates 😇")
+        await app.send_message("me", f"**Pyrogram String Session:** \n`{PYRO_SESSION}` \n\n**Powered by @NexaBotsUpdates 😇**")
         print(f"Here is your Pyrogram String Session: \n {PYRO_SESSION} \n\nPro tip: Check your saved message also, Backup of this session is also saved in there :)")
 
 
