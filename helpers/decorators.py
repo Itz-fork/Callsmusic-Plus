6ea74cd10681752fa38362d0d35b418d866c680e
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-08 20:14:19 +0530

    huh

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -6,6 +6,7 @@ from pyrogram.types import Message, CallbackQuery
 
 from helpers.admins import get_administrators
 from config import SUDO_USERS
+from cache.admins import admins as a
 
 
 def errors(func: Callable) -> Callable:
@@ -32,17 +33,12 @@ def authorized_users_only(func: Callable) -> Callable:
     return decorator
 
 
-def admin_chack_cb(func):
-    @wraps(func)
-    async def gaeshit(message: Message, query: CallbackQuery):
-        administrators = await get_administrators(query.message.chat)
-        if message.from_user.id in SUDO_USERS:
-            return await func(message, query)
-        for administrator in administrators:
-            if administrator == query.from_user.id:
-                return await func(message, query)
-        else:
-            await query.answer("You Go Away, This isn't For You!", show_alert=True)
-            return
 
-    return gaeshit
+def admin_chack_cb(func: Callable) -> Callable:
+    async def decorator(client, query):
+        admemes = a.get(query.message.chat.id)
+        if query.from_user.id in admemes:
+            return await func(client, query)
+        else:
+            await cb.answer("You ain't allowed!", show_alert=True)
+            return
