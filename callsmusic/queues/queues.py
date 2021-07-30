8b97c950c30a9f5199a1eb0735e190d90de8725a
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-30 18:28:05 +0530

    sed

--- callsmusic/queues/queues.py
+++ callsmusic/queues/queues.py
@@ -1,7 +1,5 @@
-from asyncio import Queue
-from asyncio import QueueEmpty as Empty
-from typing import Dict
-
+from asyncio import Queue, QueueEmpty as Empty
+from typing import Dict, Union
 
 queues: Dict[int, Queue] = {}
 
@@ -13,7 +11,7 @@ async def put(chat_id: int, **kwargs) -> int:
     return queues[chat_id].qsize()
 
 
-def get(chat_id: int) -> Dict[str, str]:
+def get(chat_id: int) -> Union[Dict[str, str], None]:
     if chat_id in queues:
         try:
             return queues[chat_id].get_nowait()
